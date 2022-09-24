3df57233609efabf017f21c81deb279140587b76
daniel-jebarson <danieljebarson21@gmail.com>
2022-09-24 16:12:21 +0530

    Added Theme Switcher 🚀

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -7,22 +7,23 @@ import {
   Container,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+
 const PasswordInput = () => {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
+  //   const [show, setShow] = React.useState(false);
+  //   const handleClick = () => setShow(!show);
 
   return (
     <Container maxW="550px" color="white" mt="250px">
       <InputGroup size="md" w="550px">
         <Input
-          bg="#d1cbcb"
-          pr="4.5rem"
-          type={show ? "text" : "password"}
-          placeholder="Enter password"
+          bg="#e6e1e1"
+          type="text"
+          placeholder="Search Here"
+          color="black"
         />
         <InputRightElement width="3rem">
           <IconButton
-            h="2rem"
+            h="2.3rem"
             size="md"
             colorScheme="blue"
             aria-label="Search database"
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import SearchBar from "../components/SearchBar";
+import ThemeSwitcher from "../components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <div>
+      <ThemeSwitcher />
       <SearchBar />
     </div>
   );
