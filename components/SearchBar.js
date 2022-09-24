3a8bf9a90df19a4ca29a820a7d6197db82da478b
daniel-jebarson <danieljebarson21@gmail.com>
2022-09-24 19:25:38 +0530

    Added Enter key event in SearchBar Component 💨

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -6,33 +6,43 @@ import {
   IconButton,
   Container,
 } from "@chakra-ui/react";
+
+import { useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
 
-const PasswordInput = () => {
-  //   const [show, setShow] = React.useState(false);
-  //   const handleClick = () => setShow(!show);
-
+function SearchBar() {
+  const [query, setQuery] = useState("");
+  const logging = () => {
+    console.log(query);
+  };
   return (
-    <Container maxW="550px" color="white" mt="250px">
+    <Container maxW="550px" color="white" mt="150px">
       <InputGroup size="md" w="550px">
         <Input
           bg="#e6e1e1"
           type="text"
           placeholder="Search Here"
           color="black"
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              logging();
+            }
+          }}
         />
         <InputRightElement width="3rem">
           <IconButton
             h="2.3rem"
             size="md"
             colorScheme="blue"
-            aria-label="Search database"
+            aria-label="Get request"
             icon={<SearchIcon />}
+            onClick={logging}
           />
         </InputRightElement>
       </InputGroup>
     </Container>
   );
-};
+}
 
-export default PasswordInput;
+export default SearchBar;
