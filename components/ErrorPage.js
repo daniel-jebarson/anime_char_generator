1a163a636174a50da689931aecc7ba9b31e5bd28
daniel-jebarson <danieljebarson21@gmail.com>
2022-09-25 01:18:04 +0530

    Styled error page 🚀

--- components/ErrorPage.js
+++ components/ErrorPage.js
@@ -1,5 +1,25 @@
+import {
+  Center,
+  Divider,
+  Container,
+  Badge,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react";
+
 function ErrorPage() {
-  return <div>Sorry this anime does not exist!</div>;
+  return (
+    <Center height="50px" gap={"25px"} mt="370px">
+      <Badge lg="1" fontSize="20px" colorScheme="red">
+        404
+      </Badge>
+      <Divider orientation="vertical" />
+      <Alert status="error" maxW={"250px"}>
+        <AlertIcon />
+        NO SUCH ANIME EXIST!
+      </Alert>
+    </Center>
+  );
 }
 
 export default ErrorPage;
